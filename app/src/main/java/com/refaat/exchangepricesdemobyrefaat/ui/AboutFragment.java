16806d5e5faf75e7036eb36ad1a9767e2354ee5b
Mohamed Refaat <muhamedmahmmod@gmail.com>
2021-12-20 15:45:04 +0400

    add the about info

--- app/src/main/java/com/refaat/exchangepricesdemobyrefaat/ui/AboutFragment.java
+++ app/src/main/java/com/refaat/exchangepricesdemobyrefaat/ui/AboutFragment.java
@@ -3,6 +3,7 @@ package com.refaat.exchangepricesdemobyrefaat.ui;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -12,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.refaat.exchangepricesdemobyrefaat.R;
+import com.refaat.exchangepricesdemobyrefaat.databinding.FragmentAboutBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,35 +22,10 @@ import com.refaat.exchangepricesdemobyrefaat.R;
  */
 public class AboutFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    FragmentAboutBinding binding;
 
     public AboutFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AboutFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AboutFragment newInstance(String param1, String param2) {
-        AboutFragment fragment = new AboutFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        super(R.layout.fragment_about);
     }
 
     @Override
@@ -63,11 +40,11 @@ public class AboutFragment extends Fragment {
         menu.clear();
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding = FragmentAboutBinding.bind(view);
+
+
     }
 }
